df599d3b921e27bef44b3a0c27c1e6b3028ecf6c
DotSnuk <hampusfarner@outlook.com>
2023-07-08 08:00:54 +0200

    last operation function added

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let firstNum, secondNum, displayValue, oper;
+let newNumber = false;
+oper = '';
 displayValue = 0;
-const buttons = document.querySelectorAll('.button');
-buttons.forEach(b => {
-    b.addEventListener('click', e => {
-        console.log(e.target.id);
-    }
-    );
-})
+// const buttons = document.querySelectorAll('.button');
+// buttons.forEach(b => {
+//     b.addEventListener('click', e => {
+//         console.log(e.target.id);
+//     }
+//     );
+// })
 
 // numbers
 const numbs = document.querySelectorAll('.number');
@@ -19,7 +21,11 @@ numbs.forEach(num => {
 // operators
 const ops = document.querySelectorAll('.op');
 ops.forEach(o => {
-    firstNum = parseInt(document.getElementById('current').innerText);
+    o.addEventListener('click', e => {
+        firstNum = parseInt(document.getElementById('current').innerText);
+        oper = e.target.id;
+        newNumber = true;
+    })
 })
 
 // clear
@@ -28,29 +34,66 @@ clear.addEventListener('click', e => {
     document.getElementById('current').innerText = 0;
 })
 
+// equal
+const equal = document.querySelector('#equal');
+equal.addEventListener('click', e => {
+    secondNum = parseInt(document.getElementById('current').innerText);
+    operate(firstNum, secondNum, oper);
+    lastOperation();
+})
+
+function lastOperation(){
+    const previous = document.querySelector('#previous');
+    let o;
+    switch(oper){
+        case 'add':
+            o = '+';
+            break;
+        case 'sub':
+            o = '-';
+            break;
+        case 'mul':
+            o = 'x';
+            break;
+        case 'div':
+            o = '/';
+            break;
+        default:
+            break;
+    }
+    previous.innerText = firstNum + o + secondNum;
+}
+
+
 
 function updateValue(i){
     const value = document.getElementById('current').innerText;
-    value === '0' ? document.getElementById('current').innerText = i :
+    if (newNumber === false){
+        value === '0' ? document.getElementById('current').innerText = i :
                      document.getElementById('current').innerText += i;
+    } else {
+        document.getElementById('current').innerText = i;
+        newNumber = false;
+    }
+    
 }
 
 
 
 function add(a, b){
-    return a + b;
+    document.getElementById('current').innerText = a + b;
 }
 
 function subtract(a, b){
-    return a - b;
+    document.getElementById('current').innerText = a - b;
 }
 
 function multiply(a, b){
-    return a * b;
+    document.getElementById('current').innerText = a * b;
 }
 
 function divide(a, b){
-    return a / b;
+    document.getElementById('current').innerText = a / b;
 }
 
 function operate(a, b, op){
